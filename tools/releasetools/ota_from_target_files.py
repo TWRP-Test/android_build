34713be36b3cbc8a2f72cf78514a43d31b1ffde6
Sandeep Dhavale <dhavale@google.com>
2025-09-12 12:42:13 -0700

    ota_from_target_files: add option to disable ublk from manifest
    Add option --disable_ublk which help generate OTA which will force
    dm-user based OTA on ublk enabled device.
    
    Bug: 444696988
    Test: th
    Test: manual OTA generation and apply on UBLK enabled device config
    
    Change-Id: I24d2c6c614eeb6039fc3f538d45690c13868ca43
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -268,6 +268,8 @@ A/B OTA specific options
   --full_ota_partitions
       Specify list of partitions should be updated in full OTA fashion, even if
       an incremental OTA is about to be generated
+  --disable_ublk
+      Disable ublk based OTA forcing to use dm-user even though device is ublk enabled.
 """
 
 from __future__ import print_function
@@ -342,6 +344,7 @@ OPTIONS.max_threads = None
 OPTIONS.vabc_cow_version = None
 OPTIONS.compression_factor = None
 OPTIONS.full_ota_partitions = None
+OPTIONS.disable_ublk = False
 
 
 POSTINSTALL_CONFIG = 'META/postinstall_config.txt'
@@ -1345,6 +1348,8 @@ def main(argv):
     elif o == "--full_ota_partitions":
       OPTIONS.full_ota_partitions = set(
           a.strip().strip("\"").strip("'").split(","))
+    elif o == "--disable_ublk":
+      OPTIONS.disable_ublk = True
     else:
       return False
     return True
@@ -1396,6 +1401,7 @@ def main(argv):
                                  "vabc_cow_version=",
                                  "compression_factor=",
                                  "full_ota_partitions=",
+                                 "disable_ublk",
                              ], extra_option_handler=[option_handler, payload_signer.signer_options])
   common.InitLogging()
 
@@ -1541,6 +1547,11 @@ def main(argv):
                        " detected. Please only pass in this flag if you want a"
                        " SPL downgrade. Target SPL: {} Source SPL: {}"
                        .format(target_spl, source_spl))
+  if OPTIONS.disable_ublk:
+    logger.info("Disabling UBLK as requested")
+    args[0] = ModifyTargetFilesDynamicPartitionInfo(
+        args[0], "disable_ublk", "true")
+
   if generate_ab:
     GenerateAbOtaPackage(
         target_file=args[0],
